d1c94ce955b0b3b6cab34b020129e46c7dc9118d
Luca Simione <luca.simione@gmail.com>
2018-10-07 19:13:06 +0200

    Update www/js/tools.js

--- www/js/tools.js
+++ www/js/tools.js
@@ -34,6 +34,7 @@ function functionLogout() {
 function sendAction() {
     // Check if browser is connected, otherwise don't send the data
     if (navigator.onLine) {
+        this.setLogInfo();
         var form_data = new FormData(document.getElementById("action_form"));
         $.ajax({
             url: "http://www.mindfulness-istc.online/php_scripts/sendAction.php",
@@ -62,7 +63,8 @@ function sendAction() {
 
 // Set the information for text local log
 function setLogText() {
-    document.getElementById("log_text").value = document.getElementById("m_sent").value + " \t" + document.getElementById("m_id").value + " \t" + document.getElementById("m_username").value + " \t" + document.getElementById("m_week").value + " \t" + document.getElementById("m_day").value + " \t" + document.getElementById("m_device").value + " \t" + document.getElementById("m_action").value + " \t" + document.getElementById("m_date").value + " \t" + document.getElementById("m_month").value  + " \t" + document.getElementById("m_year").value + " \t" + document.getElementById("m_hour").value + " \t" + document.getElementById("m_min").value  + " \t" + document.getElementById("m_sec").value + " \t" + document.getElementById("m_timestring").value;
+    document.getElementById("log_text").value = document.getElementById("m_sent").value + " \t" + document.getElementById("m_id").value + " \t" + document.getElementById("m_username").value + " \t" + document.getElementById("m_week").value + " \t" + document.getElementById("m_day").value + " \t" + document.getElementById("m_device").value + " \t" + document.getElementById("m_action").value + " \t" + document.getElementById("m_date").value + " \t" + document.getElementById("m_month").value  + " \t" + document.getElementById("m_year").value + " \t" + document.getElementById("m_hour").value + " \t" + document.getElementById("m_min").value  + " \t" + document.getElementById("m_sec").value + " \t" + document.getElementById("m_timestring").value + document.getElementById("m_playingTrack").value + "\t" +
+    document.getElementById("m_progressTrack").value;
 }
 
 function setLogInfo() {

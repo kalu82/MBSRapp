63e0f6e564c2730e1fb99e58bd2faa7fd0ebf1ce
kalu82 <luca.simione@gmail.com>
2018-10-08 14:55:14 +0200

    new commit

--- www/js/checksController.js
+++ www/js/checksController.js
@@ -16,20 +16,24 @@ function check_boxes() {
 function check_actweek(numact) {
     if (document.getElementById('actw' + numact).checked) {
         window.localStorage.setItem('week' + week + '_actw' + numact + '_done', '1');
-        sendAction('do_w' + numact);
+        document.getElementById("m_action").value = 'do_w' + numact;
+        sendAction();
     } else {
         window.localStorage.setItem('week' + week + '_actw' + numact + '_done', '0');
-        sendAction('undo_w' + numact);
+        document.getElementById("m_action").value = 'undo_' + numact;
+        sendAction();
     }
 }
 
 function check_act(numact) {
     if (document.getElementById('act' + numact).checked) {
-        window.localStorage.setItem('week' + week + '_act' + i + '_done','1');
-        sendAction('do' + numact);
+        window.localStorage.setItem('week' + week + '_act' + numact + '_done', '1');
         $('#row_act' + numact).addClass('animated pulse');
+        document.getElementById("m_action").value = 'do' + numact;
+        sendAction();
     } else {
-        window.localStorage.setItem('week' + week + '_act' + i + '_done','0');
-        sendAction('undo' + numact);
+        window.localStorage.setItem('week' + week + '_act' + numact + '_done', '0');
+        document.getElementById("m_action").value = 'undo' + numact;
+        sendAction();
     }
 }
